a315b0cd1c148604088b2c7b769923d1c9c157ab
GO Lee <golee.dev@gmail.com>
2020-11-13 19:02:51 +0900

    Updates

---
+++ precache-manifest.324f8b9e1188bd6873e31f51c396843c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c98955591230840a6a000fd8d817c6",
+    "url": "/5sungbds.com/index.html"
+  },
+  {
+    "revision": "707e95caad02da83b748",
+    "url": "/5sungbds.com/static/css/2.20bb17ac.chunk.css"
+  },
+  {
+    "revision": "fec1a40dfd36087c3f26",
+    "url": "/5sungbds.com/static/css/main.634e4817.chunk.css"
+  },
+  {
+    "revision": "707e95caad02da83b748",
+    "url": "/5sungbds.com/static/js/2.a6c7a5e3.chunk.js"
+  },
+  {
+    "revision": "a89043df82952537733028209276149a",
+    "url": "/5sungbds.com/static/js/2.a6c7a5e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec1a40dfd36087c3f26",
+    "url": "/5sungbds.com/static/js/main.f7d6d4d6.chunk.js"
+  },
+  {
+    "revision": "7a66329b3aa312487275",
+    "url": "/5sungbds.com/static/js/runtime-main.f0de5e0f.js"
+  },
+  {
+    "revision": "ca612ddec0ba2036af9c6ea963d57cde",
+    "url": "/5sungbds.com/static/media/card1.ca612dde.png"
+  },
+  {
+    "revision": "07c781bf2b1e6c676a600369e56c5d16",
+    "url": "/5sungbds.com/static/media/card2.07c781bf.png"
+  },
+  {
+    "revision": "97df0855d575b84ef67ca764663bea48",
+    "url": "/5sungbds.com/static/media/card3.97df0855.png"
+  },
+  {
+    "revision": "82c315dc3c7b961ddc0272551643fba0",
+    "url": "/5sungbds.com/static/media/card4.82c315dc.png"
+  },
+  {
+    "revision": "680e9303c3c2f721765a7cfd5f387fe6",
+    "url": "/5sungbds.com/static/media/card5.680e9303.png"
+  },
+  {
+    "revision": "aae8c8c4ef83a3d6d0919714d0129258",
+    "url": "/5sungbds.com/static/media/card6.aae8c8c4.png"
+  },
+  {
+    "revision": "1c7e108236aeca16b47d75f0890bdfd4",
+    "url": "/5sungbds.com/static/media/contact.1c7e1082.gif"
+  },
+  {
+    "revision": "c686a6666c509e232625be31162e51f1",
+    "url": "/5sungbds.com/static/media/home-building.c686a666.png"
+  },
+  {
+    "revision": "bc477e23887e03341c8708f7c0d73444",
+    "url": "/5sungbds.com/static/media/home-circle2.bc477e23.png"
+  },
+  {
+    "revision": "33d9b8d8a5b224b42ae4c4b98bac038b",
+    "url": "/5sungbds.com/static/media/home-circle3.33d9b8d8.png"
+  },
+  {
+    "revision": "7c40f63d0f38446329d6fe7bb352d985",
+    "url": "/5sungbds.com/static/media/icon.7c40f63d.png"
+  },
+  {
+    "revision": "46d6b83bf273e5bae8125135fa34e05d",
+    "url": "/5sungbds.com/static/media/link2.46d6b83b.png"
+  },
+  {
+    "revision": "1ed24f092d83be304d4ebc2e02386fb5",
+    "url": "/5sungbds.com/static/media/link3.1ed24f09.png"
+  },
+  {
+    "revision": "e28d511e91df703f2ff4d97b5450ae26",
+    "url": "/5sungbds.com/static/media/link4.e28d511e.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/5sungbds.com/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/5sungbds.com/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/5sungbds.com/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/5sungbds.com/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "928acf45a2fd9be2a336f1016d1bcce3",
+    "url": "/5sungbds.com/static/media/sold.928acf45.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ohsung-realestate/precache-manifest.b54bbb0353faac75c7e0affeac6cc84b.js"
+  "/5sungbds.com/precache-manifest.324f8b9e1188bd6873e31f51c396843c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ohsung-realestate/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/5sungbds.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
